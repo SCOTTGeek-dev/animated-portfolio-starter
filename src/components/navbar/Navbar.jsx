1e5c70f5b55e059a41dbdb8df00fc293e6cf3850
SCOTTGeek-dev <ham.youness@gmail.com>
2025-08-02 12:09:26 +0100

    new dependencies optimized: framer-motion

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,21 @@
+import "./navbar.scss";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      {/* Sidebar */}
+      <div className="wrapper">
+        <span>Youness Dev</span>
+        <div className="social">
+            <a href="#"><img src="/facebook.png" alt="Facebook" /></a>
+            <a href="#"><img src="/instagram.png" alt="Instagram" /></a>
+             <a href="#"><img src="/facebook.png" alt="Facebook" /></a>
+            <a href="#"><img src="/instagram.png" alt="Instagram" /></a>
+      </div>
+    </div>
+</div>
+
+  )
+}
+
+export default Navbar;
