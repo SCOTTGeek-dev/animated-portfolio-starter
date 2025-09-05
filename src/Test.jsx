223f70d32d489a5c8f04017c2d43a4a166e3a11e
SCOTTGeek-dev <ham.youness@gmail.com>
2025-09-05 11:14:46 +0100

    creating the skeleton, the toggle button will be fixed (quick fix)

--- src/Test.jsx
+++ src/Test.jsx
@@ -1,16 +1,29 @@
 import {motion} from "framer-motion";
+import { useState } from "react";
+import ToggleButton from "./components/sidebar/toggleButton/toggleButton.jsx";
 
 const Test = () => {
+
+const [open,setOpen] = useState(false)
+
+  const variants = {
+    visible: {opacity: 1, x:800, transition: {type: "spring", stiffness: 200, damping: 100}},
+    hidden: {opacity: 0},
+  }
+
   return (
     <div className="course">
   <motion.div
    className="box"
-    initial={{opacity: 0.5, scale:0.5}}
-    transition={{duration: 2}}
-    whileTap={{opacity: 1, scale: 2}}
+    variants={variants}
+    //initial="hidden"
+    //animate={}
+     transition={{duration: 2}}
+     animate={open ? "visible" : "hidden"}
     ></motion.div>
-  </div> 
-  )
+    <ToggleButton setOpen={setOpen} />
+  </div>
+  );
 };
 
 export default Test;
