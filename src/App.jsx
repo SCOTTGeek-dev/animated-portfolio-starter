714d56d3c6a54ff9f6a0c3ea8b53a2789ca0c7ac
SCOTTGeek-dev <ham.youness@gmail.com>
2025-09-08 23:13:43 +0100

    added a portfolio component and fixed some bugs on app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/navbar/navbar.jsx";
 import Hero from "./components/hero/Hero.jsx";
 import Parallax from "./components/parallax/Parallax.jsx";
 import Services from "./components/services/Services.jsx";
+import Portfolio from "./components/portfolio/Portfolio.jsx";
 
 const App = () => {
   return<div>
@@ -10,12 +11,14 @@ const App = () => {
     <Navbar />
     <Hero />
     </section>
-  <section id="Services"><Parallax type="services" /></section>
+  <section id="Services">
+    <Parallax type="services" />
+  </section>
   <section><Services /></section>
-  <section id="Portfolio"><Parallax type="portfolio" /></section>
-  <section>Portfolio1</section>
-  <section>Portfolio2</section>
-  <section>Portfolio3</section>
+  <section id="Portfolio">
+    <Parallax type="portfolio" />
+    </section>
+    <Portfolio />
   <section id="Contact">Contact</section>
   </div>;
 };
--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,57 +1,88 @@
 import "./services.scss";
-import { motion } from "framer-motion";
+import { color, motion, useInView } from "framer-motion";
+import { useRef } from "react";
+
+
+const variants = {
+    initial: {
+        x:-500,
+        y:100,
+        opacity:0,
+    },
+    animate: {
+        x:0,
+        opacity:1,
+        transition: {
+             duration: 1,
+            staggerChildren: 0.1,
+        },
+    },
+};
 
 const Services = () => {
+
+    const ref= useRef();
+    const isInView= useInView(ref, { margin: "-100px" });
+
+
     return(
-        <motion.div className="services">
-         <motion.div className="textContainer">
+        <motion.div 
+        className="services" 
+        variants={variants} 
+        initial="initial" 
+        //animate="animate"
+        //whileInView={"animate"}
+        ref={ref} 
+        animate={isInView && "animate"}
+        >
+         <motion.div className="textContainer" variants={variants}>
             <p>
-                I focus on developîng modern cloud solutions that are scalable, secure, and efficient
+                I focus on developing modern cloud solutions that are scalable, secure, and efficient
                 <br /> having experience in various technologies and frameworks
             </p>
             <hr />
         </motion.div>
-        <motion.div className="titleContainer">
+        <motion.div className="titleContainer" variants={variants}>
         <div className="title">
             <img src="/people.webp" alt="" />
             <h1>
-                <b>Unique</b>
+                <motion.b whileHover={{ color: "orange" }}>Unique </motion.b>
                 Ideas
                 </h1>
         </div>
          <div className="title">
             <h1>
-                <b>For Your </b>
-                Business maybe !
+                <motion.b whileHover={{ color: "orange" }}>For Your </motion.b>
+                SaaS !
             </h1>
             <button>WHAT I DO?</button>
         </div>
         </motion.div>
-        <motion.div className="listContainer">
-        <div className="box">
+        <motion.div className="listContainer" variants={variants}>
+        <motion.div className="box" whileHover={{background:"lightgray", color:"black"}}>
             <h2>Branding</h2>
             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
             Nihil corrupti fugiat unde! Mollitia cupiditate vel deleniti
             ut nam adipisci est id reprehenderit nostrum voluptatum aperiam qui repellat, porro rerum unde?
             </p>
             <button>Go</button>
-        </div>
-          <div className="box">
+        </motion.div>
+        <motion.div className="box" whileHover={{background:"lightgray", color:"black"}}>
             <h2>Branding</h2>
             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
             Nihil corrupti fugiat unde! Mollitia cupiditate vel deleniti
             ut nam adipisci est id reprehenderit nostrum voluptatum aperiam qui repellat, porro rerum unde?
             </p>
             <button>Go</button>
-        </div>
-          <div className="box">
+        </motion.div>
+        <motion.div className="box" whileHover={{background:"lightgray", color:"black"}}>
             <h2>Branding</h2>
             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
             Nihil corrupti fugiat unde! Mollitia cupiditate vel deleniti
             ut nam adipisci est id reprehenderit nostrum voluptatum aperiam qui repellat, porro rerum unde?
             </p>
             <button>Go</button>
-        </div>
+        </motion.div>
         </motion.div>
     </motion.div>
     );
